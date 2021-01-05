a338ac3cb8e0d01225d4d1641c5b66031c25932c
drsyed1 <69309485+drsyed1@users.noreply.github.com>
2021-01-05 23:29:17 +0530

    Update status.py

--- userge/plugins/botlist/status.py
+++ userge/plugins/botlist/status.py
@@ -4,8 +4,6 @@ import datetime
 
 import pyrogram
     
-import pytz #Install it using pip(3) install pytz
-    
 user_session_string = "BQCQX5yd3ipFkANBGiquKBfn851U9-i_rl9By2pDVU59RdgBayy8-sX5ape6oPisaQDpZCgBDHQwO1BkbP6IxecUrAOgcWnfNZGt_khgJqTZIsri5X4aYjXJKW0XETfQxMhYV4ZRYfjhE_Nle8jXKnw_pOifsQ9C0HhySsgvcs3Mxrup8fW4i07fxFjCMM2iN24G9xvC90jhUs8Nib7kkdwtid_-7jell2cge_I7CKDhA9W79NJLok755_maoI1GPG2hcWGTb6w3bqr43lHNC6rsM-LR_zmLBW0OkDTrO7xD1DDNtvhsvBAaBZReTI5-m2415F45GF7r6-19oqCkBpDJUbEaEQA"
 bots = ["WhiteEyeRenameBot", "WhiteEyeURLUploaderBot", "WhiteEyeTelegraphBot", "WhiteEyeLinkToFileBot", "WhiteEyeSubtitleBot", "WhiteEyeYouTubeBot", "WhiteEyeForceSubscriberBot", "whiteeyegdrivebot", "WhiteEyeTagRemoverBot", "WhiteEyeUltraTonBot", "WhiteEyeDeleteAllBot", "WhiteEyeCompressorBot", "Miss_ArantxaBot"]
 bot_owner = ("@Mr_StarLords")
@@ -22,7 +20,7 @@ user_client = pyrogram.Client(
 async def check_status(client, message):
 first_msg = "<b>Bots Status...</b>\n\n"
 msg = await message.reply_text(first_msg, parse_mode="html")
-bots = ["@bot1","@bot2"] #List of your bots
+bots = ["WhiteEyeRenameBot", "WhiteEyeURLUploaderBot", "WhiteEyeTelegraphBot", "WhiteEyeLinkToFileBot", "WhiteEyeSubtitleBot", "WhiteEyeYouTubeBot", "WhiteEyeForceSubscriberBot", "whiteeyegdrivebot", "WhiteEyeTagRemoverBot", "WhiteEyeUltraTonBot", "WhiteEyeDeleteAllBot", "WhiteEyeCompressorBot", "Miss_ArantxaBot"] #List of your bots
 
 for bot in bots:
       checking = f"<b>⭕️ {bot} Status : ♻️</b>\n\n"
